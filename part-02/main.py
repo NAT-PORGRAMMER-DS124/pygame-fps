51b4ddf0295cffe6e7c1e2bdc22b4a0ca8f597b5
SSripilaipong <santhapon.s@siametrics.com>
2021-03-26 23:56:45 +0700

    fix moving ball from part-01

---
+++ part-02/main.py
@@ -0,0 +1,36 @@
+import pygame
+
+pygame.init()
+
+
+display = pygame.display.set_mode((1150, 700))
+
+clock = pygame.time.Clock()
+FPS = 30
+
+x = 0
+y = 350
+while True:
+    # check exit event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    # check user input
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        y -= 1
+    if pygame.key.get_pressed()[pygame.K_DOWN]:
+        y += 1
+
+    # compute
+    display.fill('white')
+    pygame.draw.circle(display, 'red', (x, y), 50)
+
+    # wait
+    clock.tick(FPS)
+
+    # display to player
+    pygame.display.update()
+
+    x += 1
